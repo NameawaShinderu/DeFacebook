b7032338a3ec66721e0181abd6ae92a98ece6c5b
NameawaShinderu <73576564+NameawaShinderu@users.noreply.github.com>
2022-06-18 19:09:24 +0530

    Update 2_deploy_migrations.js

--- migrations/2_deploy_migrations.js
+++ migrations/2_deploy_migrations.js
@@ -3,3 +3,9 @@ const defacebook = artifacts.require("dfab");
 module.exports = function(deployer) {
   deployer.deploy(defacebook);
 };
+
+const backend = artifacts.require("simplestorage");
+
+module.exports = function(deployer) {
+  deployer.deploy(backend);
+};
